3e95276b27ad9bfda286ce05d7e593780dec4b32
mwangimuringi <mwangimuringi10@gmail.com>
2024-09-04 19:10:19 +0300

    Added shadcn component to dashboard

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,74 @@
+import { ReactNode } from "react";
+import { DashboardNavigation } from "../components/dashboard/DashboardNavigation";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { CircleUser, MenuIcon } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+// import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+// import { redirect } from "next/navigation";
+// import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+// import { unstable_noStore as noStore } from "next/cache";
+
+export default async function DashboardLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  //   noStore();
+  //   const { getUser } = getKindeServerSession();
+  //   const user = await getUser();
+
+  //   if (!user || user.email !== "heshmati74@gmail.com") {
+  //     return redirect("/");
+  //   }
+
+  return (
+    <div className="flex w-full flex-col 2xl:max-w-[1440px] mx-auto px-4 sm:px-6 lg:px-8">
+      <header className="sticky top-0 flex h-16 items-center justify-between gap-4 border-b bg-white">
+        <nav className="hidden font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+          <DashboardNavigation />
+        </nav>
+
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button
+              className="shrink-0 md:hidden"
+              variant="outline"
+              size="icon"
+            >
+              <MenuIcon className="h-5 w-5" />
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left" className="w-[50%]">
+            <nav className="flex flex-col gap-6 text-lg font-medium mt-5">
+              <DashboardNavigation />
+            </nav>
+          </SheetContent>
+        </Sheet>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="secondary" size="icon" className="rounded-full">
+              <CircleUser className="w-5 h-5" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem asChild>
+              {/* <LogoutLink className="cursor-pointer">Log out</LogoutLink> */}
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </header>
+      <main className="my-5">{children}</main>
+    </div>
+  );
+}
