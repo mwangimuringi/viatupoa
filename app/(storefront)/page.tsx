1fd8b9b5665cec61ba71abbdacf841a5e78988c3
mwangimuringi <mwangimuringi10@gmail.com>
2024-10-14 18:41:19 +0300

    Created the category-selection component

--- app/(storefront)/page.tsx
+++ app/(storefront)/page.tsx
@@ -1,26 +1,14 @@
-"use client";
 
-import { useState } from "react";
-
-import { Hero } from "../components/storefront/Hero";
 import { CategorySelection } from "../components/storefront/CategorySelection";
 import { FeaturedProducts } from "../components/storefront/FeaturedProducts";
-import { TermsModal } from "../components/TermsModal";
+import { Hero } from "../components/storefront/Hero";
 
 export default function IndexPage() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const handleSignUpSuccess = () => {
-    // Logic for successful signup
-    setIsModalOpen(true); // Show the terms modal
-  };
-
   return (
     <div>
       <Hero />
       <CategorySelection />
       <FeaturedProducts />
-      <TermsModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
     </div>
   );
 }
