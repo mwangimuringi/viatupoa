14784d6ad990cd4d43eb13ad0a6d4cd8206ee704
mwangimuringi <mwangimuringi10@gmail.com>
2024-10-14 16:42:44 +0300

    Created terms of agreement modal

--- app/(storefront)/page.tsx
+++ app/(storefront)/page.tsx
@@ -1,14 +1,26 @@
+"use client";
 
+import { useState } from "react";
+
+import { Hero } from "../components/storefront/Hero";
 import { CategorySelection } from "../components/storefront/CategorySelection";
 import { FeaturedProducts } from "../components/storefront/FeaturedProducts";
-import { Hero } from "../components/storefront/Hero";
+import { TermsModal } from "../components/TermsModal";
 
 export default function IndexPage() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleSignUpSuccess = () => {
+    // Logic for successful signup
+    setIsModalOpen(true); // Show the terms modal
+  };
+
   return (
     <div>
       <Hero />
       <CategorySelection />
       <FeaturedProducts />
+      <TermsModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
     </div>
   );
 }
