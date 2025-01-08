4be7cf791433a241d1adda8eaef27087d9155681
mwangimuringi <mwangimuringi10@gmail.com>
2025-01-08 16:32:23 +0300

    Reviewed compatibility with the latest muringi branch changes.
    Co-authored-by: mwangimuringi <mwangimuringi10@gmail.com>
    Co-authored-by: Julius Mwangi <140041067+mwangimuringi@users.noreply.github.com>

--- app/components/storefront/Hero.tsx
+++ app/components/storefront/Hero.tsx
@@ -28,12 +28,12 @@ export  async function Hero() {
         {data.map((item) => (
           <CarouselItem key={item.id}>
             <div className="relative h-[60vh] lg:h-[80vh]">
-              <Image
+              {/* <Image
                 src={item.image}
                 alt="Banner Image"
                 className="object-cover w-full h-full rounded-xl"
                 fill
-              />
+              /> */}
               <div className="absolute top-6 left-6 backdrop-blur-sm text-gray-500 p-4 rounded-lg shadow-2xl transition-transform hover:scale-105">
                 <h1 className="text-xl lg:text-2xl font-bold">{item.title}</h1>
               </div>
